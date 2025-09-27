dc5a1235a9b3e5ab210ab055344c09c6b9a46771
gnar-rip <gnar.rip@proton.me>
2025-09-27 00:56:19 -0400

    chore: Tailwind v4 configured and working

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        osrsGold: "#FFD700",
+      },
+    },
+  },
+  plugins: [],
+};
